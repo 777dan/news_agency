a2d01cc18d4946ecc8c45cd4968ed9bc6e8e6a87
777dan <danya.nikitin7@gmail.com>
2023-01-19 10:56:01 +0200

    fixed problem with table creation

--- create_tables.php
+++ create_tables.php
@@ -7,13 +7,13 @@ try {
     if (!$conn->query("CREATE TABLE IF NOT EXISTS News (id INT NOT NULL AUTO_INCREMENT, username VARCHAR (100), date DATETIME, message TEXT, category VARCHAR(255), name VARCHAR(255), PRIMARY KEY (id))")) {
         throw new Exception('Error creation table News: [' . $conn->error . ']');
     }
+    if (!$conn->query("CREATE TABLE  IF NOT EXISTS Category (category VARCHAR(255), PRIMARY KEY (category))")) {
+        throw new Exception('Error creation table  Users: [' . $conn->error . ']');
+    }
     if (!$conn->query("FOREIGN KEY (category) REFERENCES category(category)")) {
         throw new Exception('Error creation table News: [' . $conn->error . ']');
     }
     if (!$conn->query("CREATE TABLE  IF NOT EXISTS Users (user_id INT NOT NULL AUTO_INCREMENT, log VARCHAR(255), pas  VARCHAR(255), PRIMARY KEY (user_id))")) {
-        throw new Exception('Error creation table  Users: [' . $conn->error . ']');
-    }
-    if (!$conn->query("CREATE TABLE  IF NOT EXISTS Category (category VARCHAR(255), PRIMARY KEY (category))")) {
         throw new Exception('Error creation table  Users: [' . $conn->error . ']');
     }
     if (!$conn->query("INSERT INTO Users (log, pas) VALUES ('pit', '$2y$10$Or1GxFp2tUzZXGb66YLhY.JPPahtNbpL9raTRtvG4hiB9ntEnYWfu'), ('admin', '$2y$10$hksmcZazEDFpoILfWe/Lru3BErOIgHo4Kg.6fhp2C.f.SFEDQq4aO')")) {
